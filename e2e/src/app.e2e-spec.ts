634fcbb634d122619f14216e3a0ce3665c6d310e
MartinSchreiner <48844193+MartinSchreiner@users.noreply.github.com>
2020-02-25 16:00:17 -0500

    updated project name and signin form tabs

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to RecipeSite!');
+    expect(page.getTitleText()).toEqual('Welcome to Recipease!');
   });
 });
