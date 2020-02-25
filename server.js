064aca0c166a988f4627e7c37c56ede08532653b
MartinSchreiner <48844193+MartinSchreiner@users.noreply.github.com>
2020-02-25 14:05:17 -0500

    forgot to update server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/RecipeSite'));
+app.use(express.static(__dirname + '/dist/Recipease'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/RecipeSite/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Recipease/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
